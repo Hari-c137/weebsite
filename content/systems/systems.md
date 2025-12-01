4e0a44a3c059c20c828705b572db515eedb4e290
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-12-01 22:01:22 +0530

    ...

---
+++ content/systems/systems.md
@@ -0,0 +1,32 @@
+---
+title: system hardware and software configuration
+
+tags:
+  - Linux
+  - NixOS
+  - Virtualization
+  - Emacs
+---
+
+
+## Hardware
+
+**DESKTOP: Skynet**  \
+status: **working**
+| Components   | Specs           |
+|:-------------|:---------------:|
+| CPU          | intel i5-12400F |
+| GPU          | RAEDON RX 7600  |
+| RAM          | 16GB DDR4       |
+| HDD1, HDD2   | 1TB, 512GB      |
+| NVME1, NVME2 | 256GB, 256GB    |
+| SATA1        | 256GB           |
+
+
+## Software
+
+this section covers the documentation and configuration details for my NixOS infrastructure [[systems/echelon]] and a minimal Voidlinux virtualized system  [[systems/voidbox]] for low-level thinkering.
+
+
+## Topology
+    
--- flake.nix
+++ flake.nix
@@ -6,27 +6,21 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
     flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+      system: let
+        pkgs = import nixpkgs {inherit system;};
       in
-      with pkgs;
-      {
-        devShells.default = mkShell rec {
-          buildInputs = [
-            # Rust
-            nodejs
-
-          ];
-        };
-      }
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              nodejs
+            ];
+          };
+        }
     );
 }
